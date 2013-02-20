6cff4d3b6cf14fa148632fdc0cf257372aef9a94
Charles Davison <charlie@powmedia.co.uk>
2013-02-20 21:39:51 +0000

    Wrap build file in template with version number

---
+++ scripts/template.js
@@ -0,0 +1,17 @@
+/*jshint expr:true eqnull:true */
+/**
+ *
+ * Backbone.DeepModel v{{version}}
+ *
+ * Copyright (c) 2013 Charles Davison, Pow Media Ltd
+ * 
+ * Improves Backbone Model support when nested attributes are used.
+ * get() and set() can take paths e.g. 'user.name'
+ *
+ * License and more information at:
+ * http://github.com/powmedia/backbone-deep-model
+ *
+ */
+
+
+{{body}}
