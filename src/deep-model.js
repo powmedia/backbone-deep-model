8401e6bd61f80d1d6b23f085ac83c68dff1b8f21
André Gaul <gaul@web-yard.de>
2013-12-06 19:33:53 +0100

    add module definition for CommonJS / browserify
    see http://dontkry.com/posts/code/browserify-and-the-universal-module-definition.html

--- src/deep-model.js
+++ src/deep-model.js
@@ -6,6 +6,9 @@
     if (typeof define === 'function' && define.amd) {
         // AMD
         define(['underscore', 'backbone'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = factory(require('underscore'), require('backbone'));
     } else {
         // globals
         factory(_, Backbone);
